df5120911c6d90f345cbcb37777e8ba766f83177
Gaurav Mehra <135465681+GarryAKAGaurav@users.noreply.github.com>
2024-07-22 12:12:56 +0530

    Update settings.py

--- Search Engine/settings.py
+++ Search Engine/settings.py
@@ -1,4 +1,4 @@
-SEARCH_KEY = "AIzaSyAOagmFRIe9pX66naOsQYqFneCuqYy_v4U"
+SEARCH_KEY = " YOUR OWN KEY PUBLISHED FROM GOOGLE SEARCH ENGINE API "
 SEARCH_ID = "c27ab142bcd024db3"
 COUNTRY = "in"
 SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&num=10&gl=" + COUNTRY
